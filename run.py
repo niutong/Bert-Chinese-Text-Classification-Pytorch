eb990251bcd52d28213ba1f8837a670c3dd15de3
niu.tong <niu.tong@immomo.com>
2022-07-19 17:58:52 +0800

    add habit train data

--- run.py
+++ run.py
@@ -14,7 +14,8 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
     # dataset = 'THUCNews'  # 数据集
-    dataset = 'GreetMsgs'  # 数据集
+    # dataset = 'GreetMsgs'  # 数据集
+    dataset = 'HabitMsgs'  # 数据集
 
     model_name = args.model  # bert
     x = import_module('models.' + model_name)
