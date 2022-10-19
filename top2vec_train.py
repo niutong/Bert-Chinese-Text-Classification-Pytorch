e092122fd8bea6e047fbae5e026e0eb3a13af7d5
niu.tong <niu.tong@immomo.com>
2022-10-19 19:49:01 +0800

    add habit train data

--- top2vec_train.py
+++ top2vec_train.py
@@ -2,13 +2,32 @@
 
 from top2vec import Top2Vec
 from sklearn.datasets import fetch_20newsgroups
+from tqdm import tqdm
+
+
+def load_dataset(path):
+    contents = []
+    with open(path, 'r', encoding='UTF-8', errors='ignore') as f:
+        for line in tqdm(f):
+            lin = line.strip()
+            if not lin:
+                continue
+
+            contents.append(line)
+
+    return contents
+
+
+
 
 if __name__ == '__main__':
 
     newsgroups = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))
 
-    model = Top2Vec(documents=newsgroups.data, speed="learn", workers=8)
+    data_set = load_dataset("/home/recommend_nearby/work_space/niu.tong/data/good_content_rate_sorted_1010_good_male.txt")
+
+    model = Top2Vec(documents=newsgroups.data, speed="fast-learn", workers=8)
 
     print("model.get_num_topics", model.get_num_topics())
     print("model.get_topic_sizes()", model.get_topic_sizes())
-    print("model.get_topics(70)", model.get_topics(70))
+    print("model.get_topics(5)", model.get_topics(5))
