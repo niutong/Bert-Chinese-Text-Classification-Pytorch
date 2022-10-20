2d2c57c9de96e2f0ea7c4bf27a3dfda8dd8a6844
niu.tong <niu.tong@immomo.com>
2022-10-20 11:42:46 +0800

    add habit train data

--- top2vec_train.py
+++ top2vec_train.py
@@ -20,12 +20,26 @@ def load_dataset(path):
 
 if __name__ == '__main__':
 
-    newsgroups = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))
+    #newsgroups = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))
 
-    data_set = load_dataset("/home/recommend_nearby/work_space/niu.tong/data/good_content_rate_sorted_1010_good_male.txt")
+    # data_set = load_dataset("/home/recommend_nearby/work_space/niu.tong/data/good_content_rate_sorted_1010_good_male.txt")
 
-    model = Top2Vec(documents=data_set, speed="learn", workers=8)
+    # model = Top2Vec(documents=data_set, speed="learn", workers=8)
 
-    print("model.get_num_topics", model.get_num_topics())
-    print("model.get_topic_sizes()", model.get_topic_sizes())
-    print("model.get_topics(5)", model.get_topics(5))
+    # model.save("/home/recommend_nearby/work_space/niu.tong/model_save/good_content_1010_male_topics")
+    model = Top2Vec.load("/home/recommend_nearby/work_space/niu.tong/model_save/good_content_1010_male_topics")
+    topic_nums = model.get_num_topics()
+
+    topic_sizes, topic_nums_array = model.get_topic_sizes()
+
+    topic_words, word_scores, topic_nums_arry_b = model.get_topics(topic_nums)
+    print("summary topic_nums:", topic_nums)
+    for topic_index in range(topic_nums):
+        topic_words_elems = topic_words[topic_index]
+        word_scores_elems = word_scores[topic_index]
+        documents, document_scores, document_ids = model.search_documents_by_topic(topic_num=topic_index, num_docs=500)
+        print("index_" + str(topic_index), "keywords",  ",".join(topic_words_elems.tolist()), "scores",
+              ",".join(word_scores_elems.tolist()))
+
+        for doc, score, doc_id in zip(documents, document_scores, document_ids):
+            print(f"index_{topic_index} document_{doc_id} Score:{score} content:{doc}")
